138df396dcaf9bc6cb2b573a7a5485a0493d962c
Nazarii Savorona <nazar.savorona@gmail.com>
2023-08-18 20:29:46 +0300

    Add entities and ws endpoints

---
+++ cmd/assistant/main/assistant.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/Its-OP/novaposhtahack/pkg/speech_assistant/azure"
+	ws "github.com/Its-OP/novaposhtahack/pkg/websocket"
+)
+
+func main() {
+	azureKey := os.Getenv("AZURE_KEY")
+	if azureKey == "" {
+		log.Fatal("AZURE_KEY environment variable is not set")
+	}
+
+	azureRegion := os.Getenv("AZURE_REGION")
+	if azureRegion == "" {
+		log.Fatal("AZURE_REGION environment variable is not set")
+	}
+
+	endpoint := fmt.Sprintf("https://%s.tts.speech.microsoft.com/cognitiveservices/v1", azureRegion)
+	azureTTS := azure.NewAzureTTS(azureKey, endpoint)
+
+	websocket := ws.NewWebSocket(azureTTS)
+
+	http.HandleFunc("/audio-to-text", websocket.AudioToTextHandler)
+	http.HandleFunc("/text-to-audio", websocket.TextToAudioHandler)
+	fmt.Println("WebSocket server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
