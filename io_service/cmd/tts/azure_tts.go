30a07a8c26d8ec18f14a25830899b51a789cea64
Andrii Yerko <aerko12@gmail.com>
2023-08-19 18:45:42 +0300

    refactor tts

--- io_service/cmd/tts/azure_tts.go
+++ io_service/cmd/tts/azure_tts.go
@@ -1,56 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
+
+	"github.com/Its-OP/novaposhtahack/pkg/websocket"
 )
 
 func main() {
 	speechKey := "b96fde6174d945ec8d91219bfc87896e"
 	speechRegion := "eastus"
 
-	url := fmt.Sprintf("https://%s.tts.speech.microsoft.com/cognitiveservices/v1", speechRegion)
-	xmlPayload := `<speak version='1.0' xml:lang='uk-UA'>
-    <voice xml:lang='uk-UA' xml:gender='Female' name='uk-UA-PolinaNeural'>
-        привіт привіт 
-    </voice>
-</speak>`
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(xmlPayload)))
+	bytes, err := websocket.NewAzureTTS(speechRegion, speechKey).ConvertTextToSpeech("привіт привіт")
 	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
+		panic(err)
 	}
 
-	req.Header.Set("Ocp-Apim-Subscription-Key", speechKey)
-	req.Header.Set("Content-Type", "application/ssml+xml")
-	req.Header.Set("X-Microsoft-OutputFormat", "audio-16khz-128kbitrate-mono-mp3")
-	req.Header.Set("User-Agent", "curl")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	// save to file
+	err = os.WriteFile("/files/output.mp3", bytes, 0644)
 	if err != nil {
-		fmt.Println("Error making request:", err)
-		return
+		panic(err)
 	}
-	defer resp.Body.Close()
-
-	// Save the response to output.mp3
-	outputFile, err := os.Create("output.mp3")
-	if err != nil {
-		fmt.Println("Error creating output file:", err)
-		return
-	}
-	defer outputFile.Close()
-
-	_, err = io.Copy(outputFile, resp.Body)
-	if err != nil {
-		fmt.Println("Error writing to output file:", err)
-		return
-	}
-
-	fmt.Println("File saved as output.mp3")
 }
