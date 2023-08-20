6ae628e38a3ab52d605368b2f02b3ea05652841d
Oleh Prostakov <prostakov.oleh@gmail.com>
2023-08-20 11:15:21 +0300

    Change header

--- web/src/App.vue
+++ web/src/App.vue
@@ -4,7 +4,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-          <h1 class="text-center">"Very" smart AI assistant</h1>
+          <h1 class="text-center">NOVA CHAT</h1>
           <AudioRecorder/>
         </div>
       </div>
